4eebb54ab79ca24b247dfcc41e398b1681fa7ea5
Winfrida Mwigilwa <winfridamwigilwa184@gmail.com>
2024-01-26 14:10:05 +0300

    Impement basic project settings

---
+++ SkinCancerDetector/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for SkinCancerDetector project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from detector import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('signup/', views.user_signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
+    path('update-profile/', views.update_profile, name='update_profile'),
+    path('password_change/done/', views.password_change_done_view, name='password_change_done'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('detect/', views.detect, name='detect'),
+    path('upload_image/', views.upload_image, name='upload_image'),
+    
+
+]+ static(settings.MEDIA_URL, document_root = settings. MEDIA_ROOT)
